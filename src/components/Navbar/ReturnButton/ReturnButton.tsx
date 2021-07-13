7d5df654418244b7f104f8e86ec2875bba77b4d4
samuelndm <samuelnmoura@gmail.com>
2021-07-12 23:29:43 -0300

    feat: add navbar/returnButton

--- src/components/Navbar/ReturnButton/ReturnButton.tsx
+++ src/components/Navbar/ReturnButton/ReturnButton.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import arrowIcon from 'assets/icons/left-arrow.png'
 import * as S from './styles'
 
 const ReturnButton = () => {
-  return <S.Container></S.Container>
+  return (
+    <S.Container>
+      <S.ArrowIcon src={arrowIcon} alt="image of back button" />
+      <S.Title>Back</S.Title>
+    </S.Container>
+  )
 }
 
 export default ReturnButton
--- src/components/Navbar/ReturnButton/styles.ts
+++ src/components/Navbar/ReturnButton/styles.ts
@@ -1,3 +1,36 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { darken } from 'polished'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  padding: 0 1.7rem;
+  width: 11.53rem;
+  height: 3.481rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 1.7425rem;
+  background-color: #eaeaea;
+  cursor: pointer;
+  opacity: 1;
+
+  &:hover {
+    background-color: ${darken(0.03, '#eaeaea')};
+  }
+`
+
+export const ArrowIcon = styled.img`
+  height: 1.74rem;
+  width: 1.74rem;
+`
+
+export const Title = styled.p`
+  ${({ theme }) => css`
+    font-family: Arial;
+    font-size: 2.2rem;
+    font-style: normal;
+    font-weight: ${theme.font.weight.normal};
+    line-height: 2.5rem;
+    letter-spacing: 0;
+    text-align: left;
+  `}
+`
