88fdee0f306cc384fdc6bd269a3c5048d83c18e2
samuelndm <samuelnmoura@gmail.com>
2021-07-13 18:17:12 -0300

    feat: add StorePage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,7 +5,7 @@ import * as P from 'pages'
 const Routes = () => {
   return (
     <Switch>
-      {/* <Route exact path="/" component={P.HomePage} /> */}
+      <Route exact path="/" component={P.StorePage} />
       <Route exact path="*" component={P.NotFoundPage} />
     </Switch>
   )
