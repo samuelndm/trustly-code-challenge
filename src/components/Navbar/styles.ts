0ec9bcec0744fac1e5f232920f994e0556f50c52
samuelndm <samuelnmoura@gmail.com>
2021-07-12 23:26:35 -0300

    style: update navbar style

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -19,14 +19,3 @@ export const Content = styled(componentsDefaultStyles.Container)`
   align-items: center;
   justify-content: space-between;
 `
-
-export const Title = styled.h1`
-  ${({ theme }) => css`
-    font-family: Arial;
-    font-size: ${theme.font.sizes.xxlarge};
-    font-weight: ${theme.font.weight.normal};
-    line-height: 3rem;
-    letter-spacing: 0;
-    text-align: center;
-  `}
-`
