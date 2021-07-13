ae66c49c85b42f6249096e3a1d6ec25414bf83d5
samuelndm <samuelnmoura@gmail.com>
2021-07-12 22:42:08 -0300

    refactor: update routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { HashRouter, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import * as P from 'pages'
 
 const Routes = () => {
   return (
-    <HashRouter basename="/">
-      <Switch>
-        {/* <Route exact path="/" component={P.HomePage} /> */}
-        <Route exact path="*" component={P.NotFoundPage} />
-      </Switch>
-    </HashRouter>
+    <Switch>
+      {/* <Route exact path="/" component={P.HomePage} /> */}
+      <Route exact path="*" component={P.NotFoundPage} />
+    </Switch>
   )
 }
 
